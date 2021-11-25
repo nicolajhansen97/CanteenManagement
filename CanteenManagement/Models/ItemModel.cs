035bb46c0582239c616d9d3c809e0da6572815df
Nicolaj Hansen <nicolajhansen97@live.dk>
2021-11-25 14:23:54 +0100

    Models + test

---
+++ CanteenManagement/Models/ItemModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CanteenManagement.Models
+{
+    class ItemModel : Models.Bindable
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; propertyIsChanged(); }
+        }
+
+        private string description;
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; propertyIsChanged(); }
+        }
+
+
+        private double price;
+
+        public double Price
+        {
+            get { return price; }
+            set { price = value; propertyIsChanged(); }
+        }
+
+        private string category;
+
+        public string Category
+        {
+            get { return category; }
+            set { category = value; propertyIsChanged(); }
+        }
+
+        private string picture;
+
+        public string Picture
+        {
+            get { return picture; }
+            set { picture = value; propertyIsChanged(); }
+        }
+
+
+
+    }
+}
